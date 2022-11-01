24111255859936202fd5d6652e5db8be6b29b1db
Edu <ealcala@agropro.farm>
2022-11-01 18:56:47 +0100

    Add sensor reading into refactor

--- StationController.py
+++ StationController.py
@@ -1,5 +1,7 @@
 import logging
 
+import Adafruit_DHT
+
 from StationParameters import StationParameters
 from StationStatus import StationStatus
 
@@ -11,9 +13,16 @@ class StationController:
     FAN_GPIO: int
     FAN_PWM_GPIO: int
 
+    # Define default times in ticks/cycles (aprox 1 sec)
+    DEFAULT_FAN_TICKS: int = 3
+    DEFAUL_SENSOR_TICKS: int = 10
+
+    MAX_TICKS: int = max(DEFAULT_FAN_TICKS, DEFAUL_SENSOR_TICKS)
+
     # internal state
     status: StationStatus = None
     parameters: StationParameters = None
+    tick: int = 0
 
     def __init__(self, temp_gpio, heat_gpio, fan_gpio, fan_pwm_gpio, parameters: StationParameters):
         logging.debug('StationController init')
@@ -25,7 +34,14 @@ class StationController:
 
     def doControlCycle(self):
         logging.debug('New controller cycle')
-        self.status = StationStatus(23.0, 90, True, 3, True)
+        # Get current tick/cycle
+        self.tick=(self.tick+1) % self.MAX_TICKS
+
+        if (self.tick%self.DEFAUL_SENSOR_TICKS == 1):
+            h, t = self.getSensorReading()
+            self.status.temperature = t
+            self.status.humidity = h
+
 
     def getExecutionStatus(self):
         logging.debug('Getting execution status')
@@ -36,7 +52,9 @@ class StationController:
         # disableFan, Heat and humidifier
         logging.debug("Tearing down station controller")
 
-
+    def getSensorReading(self):
+        sensor=Adafruit_DHT.DHT11
+        return Adafruit_DHT.read_retry(sensor, self.TEMP_GPIO)
 
 
 
