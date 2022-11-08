7e78764173b012128e29c61334bf37d5cafad84d
Eduard Alcalá-Bejarano Lucas <ealcala@veggadigital.com>
2022-11-08 20:06:46 +0100

    Added status csv log

--- StationStatus.py
+++ StationStatus.py
@@ -12,13 +12,13 @@ class StationStatus:
 
 
     # Default constructor
-    def __init__(self, temperature=0.0, humidity=0, isFanEnabled=False, fanScheduledTicks=0, isHeatEnabled=False, ifHumidifierEnabled=False, humidifierScheduledTicks=0, inRange=True):
+    def __init__(self, temperature=0.0, humidity=0, isFanEnabled=False, fanScheduledTicks=0, isHeatEnabled=False, isHumidifierEnabled=False, humidifierScheduledTicks=0, inRange=True):
         self.temperature=temperature
         self.humidity=humidity
         self.isFanEnabled=isFanEnabled
         self.fanScheduledTicks=fanScheduledTicks
         self.isHeatEnabled=isHeatEnabled
-        self.ifHumidifierEnabled=ifHumidifierEnabled
+        self.ifHumidifierEnabled=isHumidifierEnabled
         self.humidifierScheduledTicks=humidifierScheduledTicks
         self.inRange = inRange
 
